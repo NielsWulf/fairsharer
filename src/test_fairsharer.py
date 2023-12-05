57bf967ef8b0eeabec3c876ccb14f6d56fe0a4aa
NielsWulf <38984667+NielsWulf@users.noreply.github.com>
2023-12-05 22:35:03 +0100

    Update test_fairsharer.py
    Testing V2

--- src/test_fairsharer.py
+++ src/test_fairsharer.py
@@ -3,3 +3,5 @@ from fairsharer import fair_sharer
 def test_fair_sharer():
     assert fair_sharer([0, 1000, 800, 0], 1) == [100, 800, 900, 0]
     assert fair_sharer([0, 1000, 800, 0], 2) == [100, 890, 720, 90]
+    result = fair_sharer([0, 1000, 800, 0], 1)
+    assert np.allclose(result, np.array([100, 800, 900, 0]), atol=1e-5, rtol=1e-5)
